3d43bc51a75c45df55cc6720a8d934698bd3f5ed
Dominik Schwabe <dominik.s01@gmx.de>
2024-05-29 20:25:37 +0200

    Pow Sonderfälle behandelt und Testfälle

--- Java2AB6/src/operator/binary/PowOperator.java
+++ Java2AB6/src/operator/binary/PowOperator.java
@@ -12,7 +12,8 @@ import exception.IllegalUserInputException;
  */
 public class PowOperator extends BinaryOperator
 {
-   public final static double MAX_EXPONENT = 1E9;
+   private static final double MAX_EXPONENT = 1E6;
+   private static final double MIN_EXPONENT = -1E4;
    /*
     * (non-Javadoc)
     * @see operator.binary.BinaryOperator#eval(double, double)
@@ -21,31 +22,35 @@ public class PowOperator extends BinaryOperator
    protected double eval(double x, double y)
    {
       if(!Double.isFinite(x) || !Double.isFinite(y)) {
-         throw new IllegalUserInputException("PowOperator: Zahlen müssen endlich sein.");
+         throw new IllegalArgumentException("PowOperator: Zahlen müssen endlich sein.");
       }
       
-      if(Math.abs(x) >= MANTISSA_MAX_VALUE.doubleValue()) {         
-         throw new IllegalUserInputException(
+      // Exponenten wurden so begrenzt, dass die Laufzeit nicht unfassbar lange dauert
+      // (siehe Dokumentation BigDecimal.pow())
+      if(x > MAX_EXPONENT || x < MIN_EXPONENT)
+         throw new IllegalArgumentException("PowOperator: Exponenten dürfen höchstens 1E6 oder -1E4 sein.");
+      
+      if(Math.abs(y) >= MANTISSA_MAX_VALUE.doubleValue()) {         
+         throw new IllegalArgumentException(
                "PowOperator: Basis darf höchstens (2^53)-1 ins positive oder negative sein.");
       }
-      
-      if(Math.abs(y) >= MAX_EXPONENT) {
-         throw new IllegalUserInputException(
-               "PowOperator: Exponent darf höchsten eine Millarde sein.");
-      }   
-      
       
       // Ergebnis vorher überprüfen, ob es im Wertebereich liegt      
       BigDecimal b = BigDecimal.valueOf(y);
       BigDecimal result = null;
       if(x < 0) {
+         
+         if(Math.abs(y) < MINIMUM)
+            throw new IllegalArgumentException("PowOperator: Division by 0 not allowed!");
+         
          //y^-x == 1/y^x
-         result = BigDecimal.ONE.divide(b.pow((int)Math.floor(Math.abs(x))), MathContext.DECIMAL64);
+         // In int casten, da pow nur int akzeptiert bei BigDecimal
+         result = BigDecimal.ONE.divide(b.pow((int)Math.floor(Math.abs(x))));
       } else {
          result = b.pow((int)Math.floor(x));
       }
       if (result.abs().compareTo(MANTISSA_MAX_VALUE) >= 0) {         
-         throw new IllegalUserInputException("PowOperator: Ergebnis darf höchstens (2^53)-1 sein.");
+         throw new IllegalArgumentException("PowOperator: Ergebnis darf höchstens (2^53)-1 sein.");
       }
       
       return Math.pow(y, x);
--- Java2AB6/test/operator/binary/PowOperatorTest.java
+++ Java2AB6/test/operator/binary/PowOperatorTest.java
@@ -1,8 +1,11 @@
 package operator.binary;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 /**
  * Testet den Exponential-Operator.
@@ -12,10 +15,360 @@ import org.junit.jupiter.api.Test;
 class PowOperatorTest
 {
 
+   private static PowOperator pow = new PowOperator();
+   
+   @DisplayName("Teste Grenze Unten")
    @Test
-   void test()
+   void testGrenzeUnten()
    {
-      fail("Not yet implemented");
+      assertThrows(IllegalArgumentException.class, new Executable() {
+               
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-Double.MAX_VALUE, 1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-Double.MAX_VALUE+1, 1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-Double.MAX_VALUE+2, 1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, -Double.MAX_VALUE);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, -Double.MAX_VALUE+1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, -Double.MAX_VALUE+2);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-Double.MAX_VALUE, -Double.MAX_VALUE);
+         }
+      });
+   }
+   
+   @DisplayName("Teste Grenze Unten Übergang Basis")
+   @Test
+   void testGrenzeUntenUebergangBasis()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, -Math.pow(2, 53)-1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, -Math.pow(2, 53));
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, -Math.pow(2, 53)-1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, -Math.pow(2, 53));
+         }
+      });
+   }
+   
+   @DisplayName("Teste Grenze Unten Übergang Exponent")
+   @Test
+   void testGrenzeUntenUebergangExp()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1E4-2, 1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1E4-1, 1);
+         }
+      });
+   }
+   
+   @DisplayName("Teste Funktion Unten Basis")
+   @Test
+   void testFunktionUntenBasis()
+   {
+      assertEquals(-(Math.pow(2, 53))+1, pow.eval(1, -(Math.pow(2, 53))+1));
+      assertEquals(-(Math.pow(2, 53))+2, pow.eval(1, -(Math.pow(2, 53))+2));
+      assertEquals(1, pow.eval(0, -(Math.pow(2, 53))+1));
+      assertEquals(1, pow.eval(0, -(Math.pow(2, 53))+2));
+   } 
+   
+   @DisplayName("Teste Funktion Unten Exponenten")
+   @Test
+   void testFunktionUntenExp()
+   {
+      assertEquals(1, pow.eval(-1E4, 1));
+      assertEquals(1, pow.eval(-1E4+1, 1));
+      assertEquals(1, pow.eval(-1E4+2, 1));
+   } 
+   
+   @DisplayName("Teste Funktion Mitte")
+   @Test
+   void testFunktionMitte()
+   {
+      assertEquals(1, pow.eval(-2, 1));
+      assertEquals(1, pow.eval(-1, 1));
+      assertEquals(1, pow.eval(0, 1));
+      
+      assertEquals(1, pow.eval(1, 1));
+      
+      assertEquals(-2, pow.eval(1, -2));
+      assertEquals(-1, pow.eval(1, -1));
+      assertEquals(0, pow.eval(1, 0));
+      
+   }
+   
+   @DisplayName("Teste Funktion Grenze Mitte (Basis 0 && Exponent negativ)")
+   @Test
+   void testGrenzeMitte()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1E4, 0);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1E4+1, 0);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, 0);
+         }
+      });
+   }
+   
+   @DisplayName("Teste Funktion Oben Exponenten")
+   @Test
+   void testFunktionObenExp()
+   {
+      assertEquals(1, pow.eval(1E6, 1));
+      assertEquals(1, pow.eval(1E6-1, 1));
+      assertEquals(1, pow.eval(1E6-2, 1));
+      
+      assertEquals(0, pow.eval(1E6, 0));
+      assertEquals(0, pow.eval(1E6-1, 0));
+      assertEquals(0, pow.eval(1E6-2, 0));
+   }
+   
+   @DisplayName("Teste Funktion Oben Basis")
+   @Test
+   void testFunktionObenBasis()
+   {
+      assertEquals(Math.pow(2, 53)-1, pow.eval(1, Math.pow(2, 53)-1));
+      assertEquals(Math.pow(2, 53)-2, pow.eval(1, Math.pow(2, 53)-2));
+      assertEquals(1, pow.eval(0, Math.pow(2, 53)-1));
+      assertEquals(1, pow.eval(0, Math.pow(2, 53)-2)); 
+   }
+   
+   @DisplayName("Teste Grenze Oben Übergang Exponent")
+   @Test
+   void testGrenzeObenUebergangExp()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1E6+2, 1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1E6+1, 1);
+         }
+      });
+   }
+   
+   @DisplayName("Teste Grenze Unten Übergang Basis")
+   @Test
+   void testGrenzeObenUebergangBasis()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, Math.pow(2, 53)+1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(1, Math.pow(2, 53));
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, Math.pow(2, 53)+1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, Math.pow(2, 53));
+         }
+      });
+   }
+   
+   @DisplayName("Teste Grenze Oben")
+   @Test
+   void testGrenzeOben()
+   {
+      assertThrows(IllegalArgumentException.class, new Executable() {
+               
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(Double.MAX_VALUE, -1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(Double.MAX_VALUE-1, -1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(Double.MAX_VALUE-2, -1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, Double.MAX_VALUE);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, Double.MAX_VALUE-1);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(-1, Double.MAX_VALUE-2);
+         }
+      });
+      
+      assertThrows(IllegalArgumentException.class, new Executable() {
+         
+         @Override
+         public void execute() throws Throwable
+         {
+            pow.eval(Double.MAX_VALUE, Double.MAX_VALUE);
+         }
+      });
    }
 
 }
